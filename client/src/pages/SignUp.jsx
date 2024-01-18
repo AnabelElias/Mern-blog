56433feb59fbc05d912da6cb3473637d77e6ccde
Elias <etsegay@miu.edu>
2024-01-18 10:49:10 -0500

    signup functionality added

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,14 +1,53 @@
-import { Button, Label, TextInput } from 'flowbite-react';
-import { Link } from 'react-router-dom';
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { Link, useNavigate } from 'react-router-dom';
+import {useState} from "react"
 
-const SignUp = () => {
+ const SignUp = () => {
+  const [formData,setFormData]=useState({})
+  const [errorMessage,setErrorMessage]=useState(null)
+  const [loading,setLoading]=useState(false)
+
+  const navigate=useNavigate();
+  const handleChange=(e)=>{
+    setFormData({...formData,[e.target.id]:e.target.value.trim()})
+  }
+ 
+  const handlSubmit=async(e)=>{
+    e.preventDefault()
+    if(!formData.username||!formData.email||!formData.password){
+      return setErrorMessage('please fill out all fields.')
+    }
+    try {
+      setLoading(true)
+      setErrorMessage(null)
+      const res=await fetch("/api/auth/signup",{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'},
+      body:JSON.stringify(formData)
+    });
+    const data=await res.json()
+    if(data.success===false){
+      return setErrorMessage(data.message)
+    }
+    setLoading(false)
+    if(res.ok) {
+      navigate('/signin');
+    }
+    } catch (error) {
+      setErrorMessage(error.message)
+      setLoading(false)
+    }
+    
+   
+  }
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl gap-5 mx-auto flex-col md:flex-row md:items-center">
         {/* left */}
         <div className="flex-1">
-          <Link to="/" className=" whitespace-nowrap font-bold text-4xl">
-            <span
+          <Link to='/' className='font-bold dark:text-white text-4xl'>
+                      <span
               className="px-2 py-1 bg-gradient-to-r
              from-indigo-500 via-purple-500 to-pink-500 
              rounded-lg text-white"
@@ -23,26 +62,45 @@ const SignUp = () => {
         </div>
         {/* right */}
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form  className="flex flex-col gap-4" onSubmit={handlSubmit}>
             <div>
               <Label value="Your username" />
-              <TextInput type="text" placeholder=" Username" id="username" />
+              <TextInput type="text" placeholder=" Username" id="username" onChange={handleChange} />
             </div>
             <div>
               <Label value="Your email" />
-              <TextInput type="text" placeholder=" name@company.com" id="email" />
+              <TextInput type="email onChange={handleChange}" placeholder=" name@company.com" id="email" onChange={handleChange} />
             </div>
             <div>
               <Label value="Your password" />
-              <TextInput type="text" placeholder=" password" id="password" />
+              <TextInput type="password" placeholder=" password" id="password" onChange={handleChange} />
             </div>
-            <Button gradientDuoTone='purpleToPink'>Submit</Button>
+            <Button gradientDuoTone='purpleToPink'
+            type='submit'
+            disabled={loading}
+            >
+            {loading ? (
+                <>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
+                </>
+              ) : (
+                'Sign Up'
+              )}
+              
+              </Button>
           </form>
           <div className='flex gap-2 text-sm mt-5'>  
-            <p>Have an account?</p>
-          <span>
-            <Link className='text-blue-500' to='signin'>Sign In</Link>
-          </span></div>
+            <span>Have an account?</span>
+          
+            <Link  to='signin' className='text-blue-500'>Sign In</Link>
+            </div>
+             {errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )}
+          
        
         </div>
       </div>
@@ -50,4 +108,4 @@ const SignUp = () => {
   );
 };
 
-export default SignUp;
+export default SignUp
