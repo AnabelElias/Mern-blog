cb005ff0f21b93229db92a5a32d9b9238175bbe7
Elias <etsegay@miu.edu>
2024-01-18 03:21:56 -0500

    create and run the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
-
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Signin from "./pages/Signin"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import SignUp from "./pages/SignUp"
+import About from "./pages/About"
+import Home from "./pages/Home"
+import Dashboard from "./pages/Dashboard"
 const App = () => {
   return (
-    <div className="text-3xl text-red-700">
-      Hello world
-    </div>
+   <BrowserRouter>
+   <Header/>
+    <Routes>
+      <Route path="/signin" element={<Signin/>}/>
+      <Route path="/signup" element={<SignUp/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+   
+   <Footer/>
+   </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/Projects.jsx
@@ -0,0 +1,10 @@
+
+const Projects = () => {
+  return (
+    <div>
+      my projects
+    </div>
+  )
+}
+
+export default Projects
